a3f18a7448c559199c613047f6012ebd6d7b3a7e
Hyunjun Kim <jun.kim@tritondigital.com>
2022-07-18 14:43:05 -0700

    set agency / advertiser query

---
+++ server.js
@@ -0,0 +1,126 @@
+import { ApolloServer, gql } from 'apollo-server';
+import fetch from 'node-fetch';
+import { all, hash } from 'rsvp';
+import Auth from './auth';
+
+const headers = {
+  'X-Jelli-Authentication': Auth,
+};
+
+let agencies = [
+  {
+    name: 'Ad Infinitum',
+    identity: '1',
+    logoUrl: 'https://cdns3.jelli.com/ad_images/a457559d-b53e-4aa5-9290-18603939b343.png',
+    advertisers: [ 'cc359cf2-5abc-46ba-808f-8893d71ab37f' ],
+  },
+  {
+    name: 'Endicia',
+    identity: '2',
+    logoUrl: 'https://cdns3.jelli.com/ad_images/b7a1a94a-0e16-42af-bba3-a0d14ada5c3f.png',
+    address: '385 Sherman Avenue \r\nPalo Alto, CA 94306-1864',
+    advertisers: [],
+  }
+];
+
+const typeDefs = gql`
+  type Agency {
+    identity: ID!
+    name: String!
+    logoUrl: String,
+    address: String,
+    advertisers: [Advertiser!]!
+  }
+  type Advertiser {
+    identity: ID!
+    name: String!
+    active: Boolean
+  }
+  
+  type Query {
+    allAgencies: [Agency]!
+    singleAgency(identity: ID!): Agency
+    singleAdvertiser(identity: ID!): Advertiser
+  }
+
+  type Mutation {
+    createAgency(name: String!, address: String, logoUrl: String): Agency!
+    deleteAgency(identity: ID!): Boolean!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allAgencies() {
+      let promises = {};
+      agencies.forEach((agency) => {
+        const advertiserIds = agency.advertisers || [];
+        advertiserIds.forEach((identity) => {
+          const promise = fetch(`https://api.dev.jelli.com/api/advertisers/${identity}`, { headers });
+          promises[identity] = promise;
+        });
+      });
+
+      return hash(promises).then(async (response) => {
+        let results = {};
+        for (let key in response) {
+          results[key] = await response[key].json();
+        }
+        console.log(results);
+
+        return agencies.map((agency) => {
+          const advertiserIds = agency.advertisers;
+          const advertisers = [];
+          advertiserIds.forEach((identity) => {
+            const advertiser = results[identity];
+            console.log(advertiser);
+            if (advertiser) {
+              console.log()
+              advertisers.push(advertiser);
+            }
+          });
+          agency.advertisers = advertisers;
+          return agency;
+        });
+      });
+      
+      
+    },
+    singleAgency(root, { identity }) {
+      return agencies.find((agency) => agency.identity === identity);
+    },
+    singleAdvertiser(root, { identity }) {
+      return fetch(`https://api.dev.jelli.com/api/advertisers/${identity}`, { headers }).then((response) => {
+        return response.json();
+      });
+    }
+  },
+  Mutation: {
+    createAgency(root, { name, address, logoUrl }) {
+      const newAgency = {
+        identity: `${agencies.length + 1}`,
+        name,
+        address,
+        logoUrl
+      }
+      agencies.push(newAgency);
+      return newAgency;
+    },
+    deleteAgency(root, { identity }) {
+      console.log('agencies', agencies);
+      const agency = agencies.find((agency) => agency.identity === identity);
+      console.log('agency', agency);
+      if (agency) {
+        agencies = agencies.filter((agency) => agency.identity === identity);
+        return true;
+      }
+      return false;
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`running on ${url}`);
+});
